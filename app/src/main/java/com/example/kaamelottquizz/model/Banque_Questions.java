7e58ef398538b427a4c4830a34f7e81d36b85588
Arnaud Lecuyer <arnaud.lecuyer@condorcet.be>
2020-11-10 16:13:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kaamelottquizz/model/Banque_Questions.java
@@ -0,0 +1,33 @@
+package com.example.kaamelottquizz.model;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Banque_Questions {
+
+    private List<Question> mListeQuestion;
+    private int mNQind;
+
+    public Banque_Questions(List<Question> ListeQuestion){
+        mListeQuestion= ListeQuestion;
+
+        Collections.shuffle(mListeQuestion);
+
+        mNQind=0;
+    }
+
+    public Question getQuestion(){
+
+        if(mNQind==mListeQuestion.size()){
+
+            mNQind=0;
+        }
+
+        return mListeQuestion.get(mNQind++);
+    }
+
+    }
+
+
+
---
+++ app/src/main/java/com/example/kaamelottquizz/model/Utilisateur.java
@@ -0,0 +1,22 @@
+package com.example.kaamelottquizz.model;
+
+public class Utilisateur {
+
+    private String mPrenom;
+
+
+    public String getmprenom(){
+        return mPrenom ;
+    }
+    public  void setmPrenom(String mPrenom){
+        this.mPrenom=mPrenom;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Utilisateur{" +
+                "mPrenom='" + mPrenom + '\'' +
+                '}';
+    }
+}
